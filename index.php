e5674cd137b9330d21f1ff7d3cf58ba6e9c585b8
Elogeek <elodiechristin@gmail.com>
2021-01-29 12:02:05 +0100

    go

--- index.php
+++ index.php
@@ -3,30 +3,30 @@
 // Premiere ligne
 
 $fruits = array("Apple", "Banana", "Orange");
-echo count($fruits) ."<br>";
-echo $fruits[1];
+print_r( count($fruits) ."<br>");
+print_r( $fruits[1]);
 
 //Deuxieme ligne
 
 
 $age = array("Peter" => "35", "Ben" => "37", "Joe" => "43");
 
-echo "<br><br>Ben is " .$age["Ben"]  . " years old.<br><br>";
+print_r( "<br><br>Ben is " .$age["Ben"]  . " years old.<br><br>");
 
 foreach($age as $x => $y) {
-    echo "<br>Key=" .$x  . ", Value=" .$y ."<br>" ;
+    print_r("<br>Key=" .$x  . ", Value=" .$y ."<br>");
 }
 
 //Troisieme ligne
 
 $colors = array("red", "green", "blue", "yellow");
-echo sort($colors);
-echo rsort($colors);
+print_r(sort($colors));
+print_r(rsort($colors));
 
 // Quatrieme ligne
 
 $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
-echo ksort($age);
+print_r( ksort($age));
 
 
 
